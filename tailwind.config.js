49057c663cdce7885ed69acd8c56dc9efdd81a3e
AbdoTarek1999-glitch <62442030+AbdoTarek1999-glitch@users.noreply.github.com>
2026-01-02 11:19:24 +0200

    Fix content path in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
     content: [
       "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}", // تأكد من أن هذا السطر موجود
+      "./src/**/*.{js,ts,jsx,tsx}", 
     ],
     theme: {
       extend: {},
